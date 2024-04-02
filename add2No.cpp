4948f6c82f9d978e8587d976118af5cc5bc3260c
BhagatCodes <164976554+BhagatCodes@users.noreply.github.com>
2024-04-02 16:26:40 +0530

    Add files via upload
    This is a c++ program for adding two numbers

---
+++ add2No.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n1 , n2;
+    cout<<"Enter First Number";
+    cin>>n1;
+    cout<<"Enter Secind Number";
+    cin>>n2;
+    int sum = n1 + n2;
+    cout<<"Sum of "<<n1<<" and "<<n2<<" is "<<sum;
+}
